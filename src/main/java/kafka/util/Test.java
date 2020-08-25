a878b29257b0773624ab94df14ef4481a7097547
liangtao <idiotao@163.com>
2020-08-25 20:31:54 +0800

    pattern demo

---
+++ src/main/java/kafka/util/Test.java
@@ -0,0 +1,20 @@
+package kafka.util;
+
+import org.apache.kafka.clients.producer.internals.ProducerBatch;
+
+import java.util.HashMap;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+//@Transactional
+public class Test {
+////    ProducerBatch
+////    HashMap
+////    ConcurrentHashMap
+////    CountDownLatch
+//    ArrayBlockingQueue
+
+}
+
+
+
