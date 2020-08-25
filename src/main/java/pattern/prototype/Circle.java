2c9593fbad524967474c8477aa5bbb92b27fdc91
liangtao <idiotao@163.com>
2020-08-25 21:40:43 +0800

    added prototype

---
+++ src/main/java/pattern/prototype/Circle.java
@@ -0,0 +1,12 @@
+package pattern.prototype;
+
+public class Circle extends Shape {
+    public Circle() {
+        type = "Circle";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
