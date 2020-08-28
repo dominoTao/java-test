fe802d3c66743599c905b09ffc80abdb937aa381
liangtao <idiotao@163.com>
2020-08-28 11:47:24 +0800

    三目运算验证

---
+++ src/main/java/mysql/Demo.java
@@ -0,0 +1,24 @@
+package mysql;
+
+import java.util.*;
+
+public class Demo {
+    public static void main(String[] args) {
+//        List<Integer> list = Arrays.asList(1,2,3,4,1,2,4,23,1,53,4,23,23,2 );
+        String str = UUID.randomUUID().toString()+"PNG";
+        String name = false ? str : "\t"+str;
+        System.out.println(name );
+//        list = distincted(list);
+//
+//        System.out.println(list );
+    }
+    public static List<Integer> distincted(List<Integer> list){
+        List<Integer> l = new ArrayList<>( list.size() );
+        for (Integer i : list) {
+            if(!l.contains(i)){
+                l.add(i);
+            }
+        }
+        return l;
+    }
+}
